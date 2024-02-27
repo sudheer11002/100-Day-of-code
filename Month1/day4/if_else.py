d4ceb2fb2c25f9332ca3e0b790ee7a32b7c04809
Sudheer <sudheer11002@gmail.com>
2024-02-27 20:49:20 +0530

    if else statements

---
+++ Month1/day4/if_else.py
@@ -0,0 +1,7 @@
+print("welcome to the rollercoaster")
+height = int(input("Waht is your hight in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
